eac43cdf4f0d5b440c36f8240f425a1a61a50ce6
Ryota Bannai <ryotala0528@gmail.com>
2021-03-18 20:09:33 +0900

    add: loops

---
+++ src/archives/loop.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+static void print()
+{
+  int v[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  for (auto x : v)
+  {
+    cout << x << endl;
+  }
+}
+
+static int count_x(char *p, char x)
+/*
+  string p の中に出現する文字 x をカウントしてその数を返す
+*/
+{
+  if (p == nullptr)
+    return 0;
+
+  int count = 0;
+  for (; *p != 0; ++p)
+    if (*p == x)
+      count++;
+  return count;
+}
+
+int main()
+{
+  print();
+  cout << "the result of count_x is: " << count_x("abcda", 'a') << endl;
+  return 0;
+}
