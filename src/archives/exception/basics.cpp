5e5bd392a001e5023957c96743bab8ae494b0999
Ryota Bannai <ryotala0528@gmail.com>
2021-03-31 12:58:52 +0900

    add: error handleing intro

---
+++ src/archives/exception/basics.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+struct Some_error
+{
+};
+
+void taskmaster()
+{
+  try
+  {
+    auto result = do_task();
+  }
+  catch (Some_error)
+  {
+    // fails to do do_task()
+  }
+}
+
+int do_task()
+{
+  int result{1};
+  if (true)
+  {
+    return result;
+  }
+  else
+  {
+    throw Some_error{};
+  }
+}
+
+int main()
+{
+  taskmaster();
+  return 0;
+}
