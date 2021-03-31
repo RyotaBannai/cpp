1831e652fa2a0286fd83facc677a078bffaba6c1
Ryota Bannai <ryotala0528@gmail.com>
2021-03-31 21:49:32 +0900

    add: impl accepting multiple types in progress

--- src/archives/exception/result/main.cpp
+++ src/archives/exception/result/main.cpp
@@ -7,15 +7,16 @@
 #include <stdexcept>
 using namespace std;
 
-template <typename E>
+template <class V, typename E>
 class Result
 {
 public:
-  int ok;
+  // TODO: change to any type so that accept all value type.
+  V ok;
   E err;
-  Result(int ok, E err) : ok{ok}, err{err} {}
-  Result(int ok) : Result{ok, E{}} {}
-  Result(E err) : Result{0, err} {}
+  Result(V ok, E err) : ok{ok}, err{err} {}
+  Result(V ok) : Result{ok, E{}} {}
+  Result(E err) : Result{V{}, err} {}
   string err_to_string()
   {
     // has_value, value_or などのヘルパーが用意されている
@@ -28,16 +29,25 @@ public:
       return "";
   }
 };
+// Build in exception type: http://www.cplusplus.com/reference/exception/exception/
+// catch exception: https://stackoverflow.com/questions/8480640/how-to-throw-a-c-exception
+// optional: https://cpprefjp.github.io/reference/optional/optional.html
+union Exception_types {
+  invalid_argument ia;
+  ~Exception_types(){};
+};
 
-Result<optional<invalid_argument>> compare(int a, int b)
+using Result_compare = Result<bool, optional<Exception_types>>;
+Result_compare compare(int a, int b)
 /* Preconcdition: both args must positive numbers */
 {
-  optional<invalid_argument> e;
-  e.emplace(invalid_argument("Received negative value"));
-  // auto e = make_optional(invalid_argument("Received negative value"));
+  // optional<exception_t> e;
+  // e.emplace(invalid_argument("Received negative value"));
+  Exception_types et = {invalid_argument("Received negative value")};
+  auto e = make_optional(et.ia);
   if (a < 0 || b < 0)
-    return Result<decltype(e)>(e);
-  return Result<decltype(e)>{a < b};
+    return Result_compare{e};
+  return Result_compare{a < b};
 }
 
 // void test_create_result()
