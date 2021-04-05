4a02d17fd8074a2b64dffce56f47c626090a0590
Ryota Bannai <ryotala0528@gmail.com>
2021-04-05 22:57:29 +0900

    add: return self reference to chain methods.

--- src/archives/abstraction/class/basics/date.cpp
+++ src/archives/abstraction/class/basics/date.cpp
@@ -14,9 +14,9 @@ class Date
 public:
   explicit Date(int dd = 1, int mm = 1, int yy = 1970) : d{dd}, m{mm}, y{yy} {};
   string string_rep() const; // 文字列表現
-  void add_year(int n);
-  void add_month(int n);
-  void add_day(int n);
+  Date &add_year(int n);
+  Date &add_month(int n);
+  Date &add_day(int n);
 };
 void Date::compute_cache_value() const
 {
@@ -40,20 +40,36 @@ ostream &operator<<(ostream &os, const Date &d)
   return os << d.string_rep();
 }
 
-void Date::add_day(int num)
+Date &Date::add_day(int num)
 {
   d += num;
+  return *this;
+}
+
+Date &Date::add_month(int num)
+{
+  m += num;
+  return *this;
+}
+
+Date &Date::add_year(int num)
+{
+  y += num;
+  return *this;
 }
 
 int main()
 {
   Date today{16, 10, 1996};
+  today.add_day(1).add_month(1).add_year(1);
+  today.add_day(1);
+  today.add_day(1);
   cout << today << endl;
 
   // Date tomorrow = today; // copy assignment // direct  initialization!
-  Date tomorrow{today}; // copy initialization
-  tomorrow.add_day(1);
-  cout << tomorrow << endl;
+  Date tomorrow = today;                        // copy initialization
+  tomorrow.add_day(1).add_month(1).add_year(1); // here doesn't work...
+  cout << today << endl;
 
   Date date_default{};
   cout << date_default << endl;
