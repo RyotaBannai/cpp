d587441b75bdc8c4834d9999e00fab9b7acabbb1
Ryota Bannai <ryotala0528@gmail.com>
2021-04-14 00:36:02 +0900

    add: c/c++ formatter settings.

--- src/archives/explicit.cpp
+++ src/archives/explicit.cpp
@@ -1,5 +1,4 @@
-class Foo
-{
+class Foo {
 public:
   Foo(int foo) : m_foo(foo){};
   int get_foo() { return m_foo; }
@@ -8,19 +7,17 @@ private:
   int m_foo;
 };
 
-void do_bar(Foo foo)
-{
-  int i = foo.get_foo();
-}
+void do_bar(Foo foo) { int i = foo.get_foo(); }
 
-int main()
-{
+int main() {
   do_bar(1);
-  /* if you pass string you'll get 'error: no matching function for call to 'do_bar',
-  note: candidate function not viable: no known conversion from 'const char [1]' to 'Foo' for 1st argument
+  /* if you pass string you'll get 'error: no matching function for call to
+  'do_bar', note: candidate function not viable: no known conversion from 'const
+  char [1]' to 'Foo' for 1st argument
 
   if you prefix Foo constructor with explicit,  you'll get note:
-  note: candidate function not viable: no known conversion from 'int' to 'Foo' for 1st argument
+  note: candidate function not viable: no known conversion from 'int' to 'Foo'
+  for 1st argument
   */
 
   return 0;
