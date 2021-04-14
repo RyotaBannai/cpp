5ccc1239096ce36c04821bc948943f11e7698f22
Ryota Bannai <ryotala0528@gmail.com>
2021-04-14 23:12:18 +0900

    add: call/ application operator.

--- src/archives/abstraction/class/operators/special_operators.cpp
+++ src/archives/abstraction/class/operators/special_operators.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <new>
 #include <string>
+#include <list>
 #include <vector>
+#include <complex>
 using namespace std;
 
 /* std::pair のベクタを管理 */
@@ -20,11 +22,38 @@ int& Assoc::operator[](const string& s) {
   return vec.back().second;
 }
 
-int main() {
+void test_out_assoc_opt() {
   Assoc values;
   string buf;
   while (cin >> buf) // 文字/ 文字列を読み取って読み取った内容を丸っと value としてカウント　
     ++values[buf];
   for (auto x : values.vec)
     cout << '{' << x.first << ',' << x.second << "}\n";
+}
+
+class Add {
+  complex<double> val;
+public:
+  Add(complex<double> c) :val{ c } {}
+  Add(double r, double i) :val{ r,i } {}
+
+  // インスタンスを関数のように扱うことができる.
+  void operator()(complex<double>& c) const { c += val; } // call operator/ application operator
+};
+
+void fn(vector<complex<double>>& vec, list<complex<double>>& lst, complex<double> z) {
+  for_each(vec.begin(), vec.end(), Add{ 2,3 }); // 全ての vec に対して, complex{2, 3} を追加
+  for_each(lst.begin(), lst.end(), Add{ z });
+}
+
+// sample
+// void test_out_call_opt() {
+//   fn(vector{ complex{1,2},complex{1,2} }, list{ complex{1,2},complex{1,2}, complex{1,2} });
+// }
+
+
+int main() {
+  // void test_out_assoc_opt();
+  auto v = vector<int>{ 2, 2 };
+  cout << v.at(0) << endl;
 }
