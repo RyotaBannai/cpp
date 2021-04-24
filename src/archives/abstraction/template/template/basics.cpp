69b76a26cd2445f28b7965385f4be46c08ff6faa
Ryota Bannai <ryotala0528@gmail.com>
2021-04-25 00:11:46 +0900

    add: template in progress

--- src/archives/abstraction/template/template/basics.cpp
+++ src/archives/abstraction/template/template/basics.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 
 template <typename C> class String {
-public
+public:
   // ...
   String(){};
   C &operator[](int n) { return ptr[n]; };
   // ...
 private:
+  char *ptr;
   static const int short_max = 15;
   int sz;
   union {
@@ -21,4 +22,44 @@ private:
   };
 };
 
-int main() {}
+template <typename T, typename Compare = std::less<T>> void sort(vector<T> &v)
+// シェルソート
+// テンプレートの引数が関数の引数から導出される
+// デフォルトの比較演算によるソートだけでなく、独自の比較演算も可能にする
+{
+  cout << typeid(T).name() << endl << "sorting...\n";
+
+  Compare cmp;
+  const size_t n = v.size();
+
+  for (int gap = n / 2; 0 < gap; gap /= 2)
+    for (int i = gap; i < n; i++)
+      for (int j = i - gap; 0 <= n; j -= gap)
+        if (cmp(v[j + gap], v[j]))
+          // {
+          //   T temp = v[j];
+          //   v[j] = v[j + gap];
+          //   v[j + gap] = temp;
+          // }
+          swap(v[j], v[j + gap]);
+};
+
+struct No_case {
+  bool operator()(const string &a, const string &b) const; // 大文字・小文字を区別しない
+};
+
+void use_sort(vector<int> &vi, vector<string> &vs)
+{
+  sort(vi);
+  sort(vs);
+  // sort<string, No_case>(vs);
+}
+
+int main()
+{
+  vector<int> v1{5, 2, 3, 4};
+  vector<string> v2{"aaa", "bbb"};
+  use_sort(v1, v2);
+  // for (auto x : v1)
+  //   cout << x << endl;
+}
