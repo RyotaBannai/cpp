7833411a8951c7ffc4438f45cca9309cbccae949
Ryota Bannai <ryotala0528@gmail.com>
2021-05-05 22:32:10 +0900

    add: wrapped up utility chap.

--- src/archives/std_libs/unitilies/type_function/basics.cpp
+++ src/archives/std_libs/unitilies/type_function/basics.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 using namespace std;
 
 // 右辺値参照・ムーブセマンティクス:
 // https://cpprefjp.github.io/lang/cpp11/rvalue_ref_and_move_semantics.html
+// https://en.cppreference.com/w/cpp/types/is_lvalue_reference
 
 template <typename T> struct Is_lvalue_reference {
   bool operator()() { return is_lvalue_reference<T>::value; }
@@ -15,9 +17,12 @@ template <typename T> struct Is_rvalue_reference {
 template <typename T> using Remove_reference = typename remove_reference<T>::type;
 
 template <typename T> void f(T &&v)
+// even rvalue implicitly converted to lvalue.
 {
   cout << "lvalue? " << (Is_lvalue_reference<T>{}() ? "true" : "false") << endl;
   cout << "rvalue? " << (Is_rvalue_reference<T>{}() ? "true" : "false") << endl;
+  // cout << "rvalue? " << (Is_rvalue_reference<Remove_reference<T> &&>{}() ? "true" : "false")
+  //      << endl; // recreate rvalue.
 
   Remove_reference<T> x = v;
   T y = v;
@@ -36,10 +41,36 @@ void use()
 {
   Result r{};
   Result &&r2{};
+  auto r3 = std::forward<Result>({});
   f(r);        // by reference lvalue
-  f(r2);       // by reference rvalue
+  f(r2);       // by reference lvalue
   f(move(r2)); // by value
+  f(r3);       // by by reference lvalue
   f(Result{}); //  by value
 }
 
-int main() { use(); }
+void use_swap()
+{
+  /*
+  template <class T>
+  void swap(T& a, T& b) {
+    T t = std::move(a);
+    a = std::move(b);
+    b = std::move(t);
+  }
+  */
+  vector<int> v1{1, 3};
+  vector<int> v2{2, 4};
+  std::swap(v1, v2);
+
+  for (auto x : v1)
+    cout << x << endl;
+  for (auto x : v2)
+    cout << x << endl;
+}
+
+int main()
+{
+  // use();
+  use_swap();
+}
